7a028aac85a79c819f02d862922cf98f83115241
JRyGithub <jryland@protonmail.com>
2021-05-19 19:02:50 +0200

    TicTacToe

--- index.js
+++ index.js
@@ -46,19 +46,30 @@ const oneOrTwoPlayer = (singleOrMulti) => {
 const game = (gameModeChosen) => {
   let playerOne;
   let playerTwo;
+
+  const buttonSinglePlayer = document.querySelector("#singlePlayer");
+  const buttonMutliPlayer = document.querySelector("#multiPlayer");
+
   switch (gameModeChosen) {
     case playerMode.singlePlayer:
       playerOne = player("Player One", "x");
       playerTwo = computer("easy", "o");
-      console.log("SinglePlayer");
+      CheckForButtonSelection("single");
+      buttonSinglePlayer.classList.toggle("selected");
       break;
     case playerMode.multiPlayer:
       playerOne = player("Player One", "x");
       playerTwo = player("Player Two", "o");
+      CheckForButtonSelection("multi");
+      buttonMutliPlayer.classList.toggle("selected");
       console.log("MultiPlayer");
       break;
     default:
       console.log("Game mode chosen is not valid.");
       break;
   }
+};
+
+function CheckForButtonSelection(selectedButton){
+
 };
