7dac11ed26b7f6cc2c8418a1509609d79f0e2b6d
JRyGithub <jryland@protonmail.com>
2021-05-27 21:30:03 +0200

    Main Logic In Place

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ const gameboard = (() => {
   let playerTurn = "X";
   let turnDone = false;
   const winningGameArrangements = new Array(
-  [1,2,3],
-  [1,4,7],
-  [1,5,9],
-  [2,5,8],
-  [3,6,9],
-  [3,5,7],
-  [4,5,6]);
+    [1, 2, 3],
+    [1, 4, 7],
+    [1, 5, 9],
+    [2, 5, 8],
+    [3, 6, 9],
+    [3, 5, 7],
+    [4, 5, 6]
+  );
   return {
     gameboardSquares,
     winningGameArrangements,
@@ -33,7 +34,7 @@ const player = (name, team) => {
   const getScore = () => score;
   const playerWins = () => score + 1;
   const move = (pmove) => {
-    moves[playerMove]= pmove+1;
+    moves[playerMove] = pmove + 1;
     playerMove++;
   };
   return {
@@ -83,60 +84,100 @@ const game = (gameModeChosen) => {
   }
 };
 
-function CheckForButtonSelection(selectedButton){
+function CheckForButtonSelection(selectedButton) {
   const buttonSinglePlayer = document.querySelector("#singlePlayer");
   const buttonMutliPlayer = document.querySelector("#multiPlayer");
 
-  if(selectedButton === "single" && !buttonMutliPlayer.classList.contains("selected")){
+  if (
+    selectedButton === "single" &&
+    !buttonMutliPlayer.classList.contains("selected")
+  ) {
     buttonSinglePlayer.classList.toggle("selected");
-  }else if (selectedButton === "multi" && !buttonSinglePlayer.classList.contains("selected")){
+  } else if (
+    selectedButton === "multi" &&
+    !buttonSinglePlayer.classList.contains("selected")
+  ) {
     buttonMutliPlayer.classList.toggle("selected");
-  }else{
+  } else {
     buttonSinglePlayer.classList.toggle("selected");
     buttonMutliPlayer.classList.toggle("selected");
   }
-};
+}
 
 //Game Start And Logic
 const gameStart = (player1, player2) => {
   var blocks = document.getElementsByClassName("block");
   let gameMoves = 1;
   blocks = [...blocks];
-  blocks.forEach(block => {
-    block.addEventListener("click", ()=>{
+  blocks.forEach((block) => {
+    block.addEventListener(
+      "click", function(){ BlockLogic(block, player1, player2, gameMoves); },{ once: true }
+    );
+  });
+};
 
+function BlockLogic(block, player1, player2, gameMoves){
         block.innerHTML = gameboard.playerTurn;
-        var blockId = block.id
-        var blockNum =blockId.replace(/\D/g,'');
+        var blockId = block.id;
+        var blockNum = blockId.replace(/\D/g, "");
         blockNum = parseInt(blockNum);
-
-        if(gameboard.playerTurn === player1.team){
+        if (gameboard.playerTurn === player1.team) {
           player1.move(blockNum);
           gameMoves++;
           gameboard.playerTurn = player2.team;
           CheckForWinOrEnd(gameMoves, player1);
-        }else{
+        } else {
           player2.move(blockNum);
           gameMoves++;
           gameboard.playerTurn = player1.team;
           CheckForWinOrEnd(gameMoves, player2);
         }
-       
-
-    })
-  });
-};
-
+      }
+//Checks for win or game over and calls reset game.
 const CheckForWinOrEnd = (gameMoves, currentPlayer) => {
-  if(gameMoves > gameboard.gameboardSquares.length){
+  let won = false;
+  if (gameMoves > gameboard.gameboardSquares.length) {
     alert("Game Over");
-  }else{
-    let won = false;
-    gameboard.winningGameArrangements.forEach(winningPattern =>{
-      winningPattern.every(x => currentPlayer.moves.includes(x));
-      if(winningPattern.every(x => currentPlayer.moves.includes(x))){
+  } else {
+    gameboard.winningGameArrangements.forEach((winningPattern) => {
+      winningPattern.every((x) => currentPlayer.moves.includes(x));
+      if (winningPattern.every((x) => currentPlayer.moves.includes(x))) {
         won = true;
+        console.log(currentPlayer.name + "wins!");
       }
     });
   }
+  if(won){
+    ResetGame();
+  }
 };
+
+//Resets game
+const ResetGame = () => {
+  var blocks = document.getElementsByClassName("block");
+  blocks = [...blocks];
+  blocks.forEach((block) => {
+    block.innerHTML = "";
+    block.remove();
+  });
+  gameboard.playerTurn = "X";
+  
+  CreateNewGameBoard();
+
+  var buttonsSelected = [...document.getElementsByClassName("selected")];
+  console.log(buttonsSelected);
+  buttonsSelected.forEach((button) => {
+    console.log(button);
+    button.classList.toggle("selected");
+  })
+}
+const CreateNewGameBoard = () =>{
+  const containerDiv = document.querySelector(".play-area");
+  const ticTacToeSquares = 9;
+  for(var i = 0; i<ticTacToeSquares; i++){
+    var block = document.createElement('div');
+    block.id= 'block_'+i;
+    block.className= 'block';
+    containerDiv.appendChild(block);
+  }
+}
